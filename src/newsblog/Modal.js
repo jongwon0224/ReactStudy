2249a1ccb2a76733f5feb461c4ac3cfcfa7d16b7
PCJ-93 <pw2900@naver.com>
2024-11-26 16:50:28 +0900

    집에서 다시

--- src/newsblog/Modal.js
+++ src/newsblog/Modal.js
@@ -1,13 +1,18 @@
 import './Modal.css';
 
-export default function Modal() {
+export default function Modal(props) {
 
     return (
 
-        <div className="modal">
+        <div className="modal" style={ {backgroundColor : props.bgColor} }>
             <h3>제목</h3>
             <p>날짜</p>
             <p>상세내용</p>
+            <button onClick={ () => {
+                let temp = [...props.news];
+                temp[0] = '긴급뉴스';
+                props.setNews(temp);
+            }}>제목 수정</button>
         </div>
 
     )
--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -6,16 +6,13 @@ export default function NewsBlog() {
 
 
     let title = 'React Study';
-
+    // 글제목 3개
     let [ news, setNews ] = useState([ '오늘의 뉴스', '어제의 뉴스', '내일의 뉴스' ]);
-    let [ num, setNum ] = useState(0);
+    // 좋아요 3개
+    let [ num, setNum ] = useState([ 0, 0, 0 ]);
     let text = '내용 무';
 
 
-    function heartClick(event) {
-        event.stopPropagation();
-        setNum(num + 1);
-    }
 
     function newsChange() {
         // setNews(news.filter((value) => {
@@ -51,27 +48,27 @@ export default function NewsBlog() {
                 <div style={{ color: 'orange', fontSize: '20px' }}>{title}</div>
             </div>
 
-            <div className='post-list'>
-                <h4 onClick={() => {onOff()}}>{news[ 0 ]} <span className='heart' onClick={(event) => { heartClick(event); }}>♥</span>
-                    <span className='font-size'>{num}</span></h4>
-                <p>{text}</p>
-            </div>
-
-            <div className='post-list'>
-                <h4>{news[ 1 ]}</h4>
-                <p>{text}</p>
-            </div>
-
-            <div className='post-list'>
-                <h4>{news[ 2 ]}</h4>
-                <p>{text}</p>
-            </div>
-
+            {
+                news.map((item, index) => {
+                    return (
+                        <div className='post-list'>
+                            <h4 onClick={() => { onOff() }}>{item}
+                                <span onClick={(event) => {
+                                    event.stopPropagation();
+                                    let temp = [ ...num ];
+                                    temp[ index ]++;
+                                    setNum(temp);
+                                }}>♥</span>
+                                <span className='font-size'>{num[ index ]}</span></h4>
+                            <p>{text}</p>
+                        </div>);
+                })
+            }
 
             <button onClick={() => { newsChange() }}>제목 변경</button>
-            {
-                modalFlag == true ? <Modal /> : null
 
+            {
+                modalFlag == true ? <Modal news={news} setNews={newsChange} bgColor={'lightGreen'} /> : null
             }
 
         </div >
